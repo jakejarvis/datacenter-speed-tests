75e4072f4b03b2e9e3a17ae44773116628bff725
Jake Jarvis <jakejarvis@gmail.com>
2019-03-11 13:17:23 -0400

    fix line breaks

--- ping.sh
+++ ping.sh
@@ -1,5 +1,5 @@
 
-echo "DigitalOcean Ping Test: \n"
+echo -e "DigitalOcean Ping Test: \n"
 
 for DC in NYC1 NYC2 NYC3 SFO1 SFO2 TOR1 LON1 FRA1 AMS2 AMS3 SGP1 BLR1
 do
@@ -7,7 +7,7 @@ do
 done
 
 echo ""
-echo "Linode Ping Test: \n"
+echo -e "Linode Ping Test: \n"
 
 for DC in Newark Atlanta Dallas Fremont London Frankfurt Singapore Tokyo2
 do
@@ -15,7 +15,7 @@ do
 done
 
 echo ""
-echo "AWS Ping Test: \n"
+echo -e "AWS Ping Test: \n"
 
 for DC in us-east-1 us-west-1 us-west-2 eu-central-1 eu-west-1 eu-west-2 ap-northeast-1 ap-south-1 ap-southeast-1 ap-southeast-2 sa-east-1
 do
@@ -23,7 +23,7 @@ do
 done
 
 echo ""
-echo "GCP Ping Test: \n"
+echo -e "GCP Ping Test: \n"
 
 for DC in us-central1 us-east1 us-east4 us-west1 northamerica-northeast1 asia-east1 asia-east2 asia-northeast1 asia-south1 asia-southeast1 australia-southeast1 europe-north1 europe-west1 europe-west2 europe-west3 europe-west4 southamerica-east1
 do
--- speed.sh
+++ speed.sh
@@ -1,4 +1,4 @@
-echo "DigitalOcean Speed Test: (100MB each) \n"
+echo -e "DigitalOcean Speed Test: (100MB each) \n"
 
 for DC in NYC1 NYC2 NYC3 SFO1 SFO2 TOR1 LON1 FRA1 AMS2 AMS3 SGP1 BLR1
 do
@@ -6,7 +6,7 @@ do
 done
 
 echo ""
-echo "Linode Speed Test: (100MB each) \n"
+echo -e "Linode Speed Test: (100MB each) \n"
 
 for DC in newark atlanta dallas fremont london frankfurt singapore tokyo2
 do
@@ -14,7 +14,7 @@ do
 done
 
 echo ""
-echo "AWS Speed Test: (10MB each) \n"
+echo -e "AWS Speed Test: (10MB each) \n"
 
 for DC in us-east-1 us-west-1 us-west-2 eu-central-1 eu-west-1 eu-west-2 ap-northeast-1 ap-south-1 ap-southeast-1 ap-southeast-2 sa-east-1
 do
@@ -22,7 +22,7 @@ do
 done
 
 echo ""
-echo "GCP Speed Test: (10MB each) \n"
+echo -e "GCP Speed Test: (10MB each) \n"
 
 for DC in us-central1 us-east1 us-east4 us-west1 northamerica-northeast1 asia-east1 asia-east2 asia-northeast1 asia-south1 asia-southeast1 australia-southeast1 europe-north1 europe-west1 europe-west2 europe-west3 europe-west4 southamerica-east1
 do
@@ -30,7 +30,7 @@ do
 done
 
 echo ""
-echo "Vultr/Choopa Speed Test: (100MB each) \n"
+echo -e "Vultr/Choopa Speed Test: (100MB each) \n"
 
 for DC in nj-us il-us ga-us fl-us tx-us sjo-ca-us wa-us tor-ca fra-de ams-nl par-fr lon-gb syd-au sgp hnd-jp
 do
