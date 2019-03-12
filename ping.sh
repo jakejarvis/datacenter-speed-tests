c0e73ac4100712b2e0921847d90293705ff728db
Jake Jarvis <jakejarvis@gmail.com>
2019-03-12 10:37:11 -0400

    rudimentary progress spinner

--- ping.sh
+++ ping.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
 
-echo -e "DigitalOcean Ping Test:\n"
+printf "DigitalOcean Ping Test:\n\n"
 
 for DC in NYC1 NYC2 NYC3 SFO1 SFO2 TOR1 LON1 FRA1 AMS2 AMS3 SGP1 BLR1
 do
-    echo "$DC: $(ping -i .2 -c 10 -q speedtest-$DC.digitalocean.com | awk -F/ '/^round|^rtt/{print $5}') ms"
+    printf "$DC: \t$(ping -i .2 -c 10 -q speedtest-$DC.digitalocean.com | awk -F/ '/^round|^rtt/{print $5}') ms\n" | expand -t 20
 done
 
-echo -e "\n\nLinode Ping Test:\n"
+
+printf "\n\nLinode Ping Test:\n\n"
 
 for DC in Newark Atlanta Dallas Fremont London Frankfurt Singapore Tokyo2
 do
-    echo "$DC: $(ping -i .2 -c 10 -q speedtest.$DC.linode.com | awk -F/ '/^round|^rtt/{print $5}') ms"
+    printf "$DC: \t$(ping -i .2 -c 10 -q speedtest.$DC.linode.com | awk -F/ '/^round|^rtt/{print $5}') ms\n" | expand -t 20
 done
 
-echo -e "\n\nAWS Ping Test:\n"
+
+printf "\n\nAWS Ping Test:\n\n"
 
 for DC in us-east-1 us-west-1 us-west-2 eu-north-1 ca-central-1 eu-central-1 eu-west-1 eu-west-2 ap-northeast-1 ap-south-1 ap-southeast-1 ap-southeast-2 sa-east-1
 do
-    echo "$DC: $(ping -i .2 -c 10 -q ec2.$DC.amazonaws.com | awk -F/ '/^round|^rtt/{print $5}') ms"
-done
-
-echo -e "\n\nGCP Ping Test:\n"
-
-for DC in us-central1 us-east1 us-east4 us-west1 northamerica-northeast1 asia-east1 asia-east2 asia-northeast1 asia-south1 asia-southeast1 australia-southeast1 europe-north1 europe-west1 europe-west2 europe-west3 europe-west4 southamerica-east1
-do
-    echo "$DC: $(ping -i .2 -c 10 -q $DC-gce.cloudharmony.net | awk -F/ '/^round|^rtt/{print $5}') ms"
+    printf "$DC: \t$(ping -i .2 -c 10 -q ec2.$DC.amazonaws.com | awk -F/ '/^round|^rtt/{print $5}') ms\n" | expand -t 20
 done
 
 
-echo -e "\n\nVultr/Choopa Ping Test:\n"
+printf "\n\nGCP Ping Test:\n\n"
 
-for DC in nj-us il-us ga-us fl-us tx-us sjo-ca-us wa-us tor-ca fra-de ams-nl par-fr lon-gb syd-au sgp hnd-jp
+for DC in us-east1 us-east4 us-west1 us-central1 northamerica-northeast1 asia-east1 asia-east2 asia-northeast1 asia-south1 asia-southeast1 australia-southeast1 europe-north1 europe-west1 europe-west2 europe-west3 europe-west4 southamerica-east1
 do
-    echo "$DC: $(ping -i .2 -c 10 -q $DC-ping.vultr.com | awk -F/ '/^round|^rtt/{print $5}') ms"
+    printf "$DC: \t$(ping -i .2 -c 10 -q $DC-gce.cloudharmony.net | awk -F/ '/^round|^rtt/{print $5}') ms\n" | expand -t 20
+done
+
+
+printf "\n\nVultr/Choopa Ping Test:\n\n"
+
+for DC in NJ-US IL-US GA-US FL-US TX-US SJO-CA-US WA-US TOR-CA FRA-DE AMS-NL PAR-FR LON-GB SYD-AU SGP HND-JP
+do
+    printf "$DC: \t$(ping -i .2 -c 10 -q $DC-ping.vultr.com | awk -F/ '/^round|^rtt/{print $5}') ms\n" | expand -t 20
 done
